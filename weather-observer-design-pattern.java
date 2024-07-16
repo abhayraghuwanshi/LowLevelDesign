81e6899061db9c8ad17d3b76b19f444b22862911
Abhay raghuwanshi <45534131+abhayraghuwanshi@users.noreply.github.com>
2024-07-16 16:57:15 +0530

    Observer Design pattern

---
+++ weather-observer-design-pattern.java
@@ -0,0 +1,83 @@
+/*
+
+Problem: Let us Imagine a scenario where the weather station is observed by various smart devices. 
+The weather station maintains a list of registered devices. When there’s a change in weather conditions, 
+the weather station notifies all devices about the update.
+
+Solution: Observer Design pattern
+
+*/
+
+public Interface WeatherStation {
+
+    void notifiesUsers(String message);
+    void addUsers(Observer user);
+    void removeUsers(Observer user):
+
+}
+
+
+public class WeatherStationImpl implements WeatherStation {
+
+    private List<Observer> users = new ArrayList<Observer>();
+    private String currState;
+
+    @overide
+    void notifiesUsers(String message){
+        for (Observer user: users){
+            user.sendMessage(message);
+        }
+    }
+
+    @overide
+    void addUsers(Observer user) {
+        users.add(user)
+    }
+    
+    @overide
+    void removeUsers(Observer user) {
+        users.remove(user)
+    }
+
+    void setCurrState(String state){
+        this.currState = state;
+        notifiesUsers(state);
+    }
+
+} 
+
+public Interface Observer {
+
+    void sendMessage(String msg);
+
+}
+
+public class ObserverImpl implements Observer {
+
+    private User user;
+
+    public ObserverImpl(User user){
+        this.user = user;
+    }
+
+    @overide
+    void sendMessage(String msg){
+        log.info("Sending to  -"+ user.name)
+        log.info("Current weather -"+ user.name)
+    }
+
+}
+
+public class User {
+
+    private String name;
+    private int id;
+
+}
+
+
+User a = User("TV", "1")
+Observer ob = new ObserverImpl(user);
+WeatherStation ws = new WeatherStation();
+ws.addUsers(a);
+ws.notifiesUsers("hello testing");
